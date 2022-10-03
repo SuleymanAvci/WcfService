166992bb0640b78e431151d2dfc8d6e53d4b790d
sa1avci <sa1avci@hotmail.com>
2022-10-03 14:04:41 +0300

    Kurlar Eklendi

--- ServiceAlmaMVC/Controllers/HomeController.cs
+++ ServiceAlmaMVC/Controllers/HomeController.cs
@@ -14,8 +14,26 @@ namespace ServiceAlmaMVC.Controllers
         {
             using (KurServis.KurServiceClient servisClient = new KurServis.KurServiceClient())
             {
+                if (TempData["Kurlar"] != null)
+                {
+                    ViewBag.Kurlar = (List<double>)TempData["Kurlar"];
+                }
+                else
+                {
+                    ViewBag.Kurlar=new List<double>();
+                }
                 var model = servisClient.ParaBirimleriGetir();
                 return View(model);
+            }
+        }
+
+        public ActionResult KurGetir(string paraBirimi)
+        {
+            using (KurServis.KurServiceClient servisClient = new KurServis.KurServiceClient())
+            {
+                var model = servisClient.KurlarGetir(paraBirimi).ToList();
+                TempData["Kurlar"] = model;
+                return RedirectToAction("Index");
             }
         }
     }
